277344f3838362631b50a920db6d476b191c6d2a
Loontek <sojjkin02@mail.ru>
2023-02-24 11:37:49 +0300

    fix: change category upload logic

--- src/store/category/categorySlice.js
+++ src/store/category/categorySlice.js
@@ -7,37 +7,32 @@ const initialState = {
   activeCategory: 0,
 };
 
-export const categoryRequestAsync = createAsyncThunk("category/fetch", (data, obj) => {
-  console.log("data: ", data);
-  console.log("obj: ", obj);
-
-  return fetch(`${API_URL}${POSTFIX}/category`)
+export const categoryRequestAsync = createAsyncThunk("category/fetch", () =>
+  fetch(`${API_URL}${POSTFIX}/category`)
     .then((req) => req.json())
-    .catch((error) => ({ error }));
-});
+    .catch((error) => ({ error }))
+);
 
 const categorySlice = createSlice({
   name: "category",
   initialState,
   reducers: {
     changeCategory(state, action) {
-      console.log("action: ", action);
-      console.log("state: ", state);
-
       state.activeCategory = action.payload.indexCategory;
     },
   },
-  extraReducers: {
-    [categoryRequestAsync.pending.type]: (state) => {
-      state.error = "";
-    },
-    [categoryRequestAsync.fulfilled.type]: (state, action) => {
-      state.error = "";
-      state.category = action.payload;
-    },
-    [categoryRequestAsync.rejected.type]: (state, action) => {
-      state.error = action.payload.error;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(categoryRequestAsync.pending.type, (state) => {
+        state.error = "";
+      })
+      .addCase(categoryRequestAsync.fulfilled.type, (state, action) => {
+        state.error = "";
+        state.category = action.payload;
+      })
+      .addCase(categoryRequestAsync.rejected.type, (state, action) => {
+        state.error = action.payload.error;
+      });
   },
 });
 
